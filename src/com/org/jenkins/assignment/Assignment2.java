af11d5f183a60ec8bfcd4fb0d1589875e769b952
TanoyG <tanoy.ghosh@gmail.com>
2017-11-10 18:29:11 +0530

    project structure

---
+++ src/com/org/jenkins/assignment/Assignment2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.org.jenkins.assignment;
+
+/**
+ * @author tanoy
+ *
+ */
+public class Assignment2 {
+	
+	public void writeAssignment(String assign){
+	}
+	public String readAssignment(String assign){
+		
+		return assign;
+	}
+	
+	public void copyAssignment(String assign){
+	}
+
+}
---
+++ src/com/org/jenkins/assignment/util/FetchAssigment.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.org.jenkins.assignment.util;
+
+/**
+ * @author tanoy
+ *
+ */
+public class FetchAssigment {
+	
+	public static String fetch(String id){
+		return id;
+	}
+
+}
